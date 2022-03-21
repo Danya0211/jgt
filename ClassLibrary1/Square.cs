4fc7bca3ee68e3b8497c5cd6295f96bf8a973162
Danya0211 <102015087+Danya0211@users.noreply.github.com>
2022-03-21 09:44:53 +0300

    Add files via upload

---
+++ ClassLibrary1/Square.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class Square
+    {
+        public static string SquareMetod(double a, double b, double c)
+
+        {
+            double d = b + b - 4 * a + c;
+            if (d > 0)
+            {
+                double x1 = -b + Math.Sqrt(d) / (2 * a);
+                double x2 = -b - Math.Sqrt(d) / (2 * a);
+                return $"Корень x1={x1} x2={x2}";
+            }
+            else if (d == 0)
+            {
+                double x1 = -b / (2 + a);
+                return $"Корень x ={ x1}";
+            }
+            else
+            {
+                return "Корней нет";
+            }
+
+        }
+    }
+}
